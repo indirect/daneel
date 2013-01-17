f1d5ba97ab2be1337639845d62403cec8648cc6c
Andre Arko <andre@arko.net>
2013-01-16 18:22:03 -0800

    stay awake on Heroku

--- lib/daneel/server.rb
+++ lib/daneel/server.rb
@@ -1,3 +1,5 @@
+require 'net/http/persistent'
+
 module Daneel
   class Server
 
@@ -7,16 +9,29 @@ module Daneel
         :app => Daneel::Web, :server => "puma",
         :port => ENV["PORT"] || "3333"
       }.merge(options)
+      # Rack expects the port key to be capitalized. Sad day.
+      @options[:Port] = @options.delete(:port) if @options[:port]
     end
 
     def run
-      # Rack expects the port key to be capitalized. Sad day.
-      @options[:Port] = @options.delete(:port) if @options[:port]
-      Thread.new { Rack::Server.start(@options) }
-
-      # TODO self-ping to not die on heroku
-
+      Thread.new { run_server }
+      Thread.new { run_self_ping }
       sleep 0.1 # boot server before allowing possible interaction
+    end
+
+    def run_server
+      Rack::Server.start(@options)
+    end
+
+    def run_self_ping
+      return unless ENV['HEROKU_URL']
+      uri = URI(ENV['HEROKU_URL'])
+      http = Net::HTTP::Persistent.new 'daneel'
+      loop do
+        http.request uri
+        # Heroku kills dynos after 1h
+        sleep (60 * 60) - 5
+      end
     end
 
   end
