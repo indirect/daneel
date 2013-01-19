347bc82ed646706caa168978ad40c6610c6e5e3a
Andre Arko <andre@arko.net>
2013-01-18 23:36:25 -0800

    shell adapter for rooms

--- lib/daneel/adapters/shell.rb
+++ lib/daneel/adapters/shell.rb
@@ -1,28 +1,34 @@
 require 'daneel/adapter'
 require 'daneel/message'
+require 'daneel/room'
 require 'readline'
 
 module Daneel
   class Adapters
     class Shell < Daneel::Adapter
 
+      def initialize(robot)
+        super
+        @room = Room.new(1, self)
+      end
+
       def run
         # End the line we were on when we exit
         trap(:EXIT){ print "\n" }
 
-        while text = Readline.readline("#{@robot.name}> ", true)
+        while text = Readline.readline("> ", true)
           next if text.empty?
-          message = Message.new(text, "shell", Time.now, "text")
-          robot.receive message
+          message = Message.new(text, @room, Time.now, "text")
+          robot.receive @room, message
         end
       end
 
-      def say(room, text)
-        say_all text
+      def say(id, *strings)
+        puts *strings
       end
 
-      def say_all(text)
-        puts text
+      def announce(*strings)
+        say @room.id, *strings
       end
 
     end
