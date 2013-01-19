bc275c38015c36b2e661454e449313a54502b6fe
Andre Arko <andre@arko.net>
2013-01-18 23:38:05 -0800

    fix option parsing optional arguments

--- lib/daneel/options.rb
+++ lib/daneel/options.rb
@@ -13,19 +13,19 @@ module Daneel
           options[:verbose] = v
         end
 
-        opts.on("-s [PORT]", "--server=[PORT]", "Run the HTTP server on PORT (default 3333)") do |port|
           options[:server] = {:port => port}
+        opts.on("-s", "--server", "[PORT]", "Run the HTTP server on PORT (default 3333)") do |port|
         end
 
-        opts.on("-a", "--adapter=NAME", "Which interaction adapter to use") do |name|
+        opts.on("-a", "--adapter ADAPTER", "Which interaction adapter to use") do |name|
           options[:adapter] = name
         end
 
-        opts.on("-n", "--name=NAME", "The name your bot should respond to") do |name|
+        opts.on("-n", "--name NAME", "The name your bot should respond to") do |name|
           options[:name] = name
         end
 
-        opts.on("-f", "--full-name=NAME", "The name your bot will use to refer to itself") do |name|
+        opts.on("-f", "--full-name FULLNAME", "The name your bot will use to refer to itself") do |name|
           options[:full_name] = name
         end
       end.parse(args)
