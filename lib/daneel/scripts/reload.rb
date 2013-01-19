dd9f9b76ec9bd3386294af263fce34b55d869791
Andre Arko <andre@arko.net>
2013-01-19 12:37:03 -0800

    reload needs to say into the room

--- lib/daneel/scripts/reload.rb
+++ lib/daneel/scripts/reload.rb
@@ -10,9 +10,10 @@ module Daneel
         when /^update$/
           return unless in_git?
           system("cd #{root} && git pull origin master && bundle install")
+          room.say "updated, brb"
           restart
         when /^reload$/
-          say "Indeed. Reloading now."
+          room.say "k, brb"
           restart
         end
       end
