cffc968fd1cbb51601a4ef73704ef5dd515187ab
Andre Arko <andre@arko.net>
2013-01-27 10:45:15 -0800

    use data to track users and rooms

--- lib/daneel/adapters/campfire.rb
+++ lib/daneel/adapters/campfire.rb
@@ -11,11 +11,10 @@ module Daneel
         domain = ENV['CAMPFIRE_SUBDOMAIN']
         token  = ENV['CAMPFIRE_API_TOKEN']
         @fire  = Sparks.new(domain, token, :logger => logger)
-        @rooms = ENV['CAMPFIRE_ROOM_IDS'].split(",").map do |id|
-          Room.new(id.to_i, self, @fire.room(id))
+
+        ENV['CAMPFIRE_ROOM_IDS'].split(",").map do |id|
+          robot.data.rooms[id.to_i] ||= Room.new(id.to_i, self, @fire.room(id))
         end
-        @users = []
-        @me = @fire.me
       end
 
       def run
@@ -47,16 +46,15 @@ module Daneel
         @rooms.each{|r| @fire.room(r.id).leave }
       end
 
-      def find_user(id)
-        return @users[id] if @users[id]
-
-        udata = @fire.user(id)
-        @users[id] = User.new(udata["id"], udata["name"], udata)
+      def me
+        @me ||= begin
+          data = @fire.me
+          me = User.new(data["id"], data["name"], data)
+          robot.data.users[me.id] = me
+        end
       end
 
-      def find_room(id)
-        @rooms.find{|r| r.id == id }
-      end
+    private
 
       def watch_room(room)
         @fire.watch(room.id) do |data|
@@ -64,15 +62,15 @@ module Daneel
 
           # TODO pass through self-messages, once they are filtered by
           # the accept? method on scripts
-          next if data["user_id"] == @me["id"]
+          next if data["user_id"] == me.id
 
           text = data["body"]
           time = Time.parse(data["created_at"]) rescue Time.now
           type = data["type"].gsub(/Message$/, '').downcase
-          message = Message.new(text, time, type)
-          room = find_room(data["room_id"])
-          user = find_user(data["user_id"])
-          robot.receive room, message, user
+          mesg = Message.new(text, time, type)
+          room = robot.data.rooms(data["room_id"])
+          user = robot.data.users(data["user_id"])
+          robot.receive room, mesg, user
         end
       end
 
