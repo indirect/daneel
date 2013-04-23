0e50e3132223b45148e53602e843f32585b7fc4c
Andre Arko <andre@arko.net>
2013-04-22 23:18:52 -0700

    turns out backslashes still escape. bah.

--- lib/daneel/scripts/dance.rb
+++ lib/daneel/scripts/dance.rb
@@ -2,6 +2,6 @@
 #
 # \o/ - Receive a happy dance gif.
 class Dance < Daneel::Script::Image
-  image_on(%r|\o/|i)
+  image_on(/\\o\//)
   images %w(http://factoryjoe.s3.amazonaws.com/emoticons/emoticon-0169-dance.gif)
 end
