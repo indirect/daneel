44196a311ef0ab899cdd5f36f22811eea6c6192b
Andre Arko <andre@arko.net>
2013-07-03 12:35:52 -0700

    less aggressive touching policy

--- lib/daneel/scripts/touching.rb
+++ lib/daneel/scripts/touching.rb
@@ -2,6 +2,6 @@
 #
 # touch, touching - Be reminded of the touching policy.
 class Touching < Daneel::Script::Image
-  image_on(/\btouch(ing)?\b/i)
+  image_on(/\bno touch(ing)?\b/i)
   images %w(http://cl.ly/image/3f2Z3H080W09/No%20touching.gif)
 end
